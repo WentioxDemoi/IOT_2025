ef2fb01c98e1fedb2336ce64951755300d581e9b
WentioxDemoi <remi.louis.desbordes@gmail.com>
2024-11-07 15:27:13 +0100

    [ADD] change HTTP to HTTPS

--- frontend/src/Host.js
+++ frontend/src/Host.js
@@ -1,3 +1,3 @@
 //export const BACKEND = 'http://172.20.10.2:3000';
-export const BACKEND = 'http://13.36.10.100:3000';
+export const BACKEND = 'https://13.36.10.100:3000';
 // Mettre IP du back (ipconfig getifaddr en0)
